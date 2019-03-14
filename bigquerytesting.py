a23ad46f23490ea4ea907c1878389dfc4dd6ccc4
Patrick McNamee <p678m854@gmail.com>
2019-03-14 16:41:50 -0500

    Added SQL commands that should work

--- bigquerytesting.py
+++ bigquerytesting.py
@@ -1,5 +1,6 @@
 from google.cloud import bigquery
 import pandas as pd
+import numpy as np
 
 #BigQuery from Google
 client = bigquery.Client()
@@ -15,4 +16,59 @@ table = client.get_table(table_ref)
 
 df = client.list_rows(table).to_dataframe()
 
-df.info()
+df.info() #Prints all rows in 1 table
+
+
+#ML attempts
+#dataset = bigquery.Dataset(ng_dataset_ref)
+#client.create_dataset(dataset)
+
+#%load_ext google.cloud.bigquery
+
+#%%bigquery
+train_query = """
+    CREATE OR REPLACE MODEL 
+    'weather_model.linear'
+    OPTIONS(model_type='linear_reg') AS
+    SELECT
+      temp,
+      dewp,
+      slp,
+      stp,
+      wdsp,
+      year,
+    FROM
+      'table'
+    """
+
+training_job = client.query(train_query)
+print(training_job)
+
+training_info = """
+  SELECT
+    *
+  FROM
+    ML.TRAINING_INFO(MODEL 'weather_model.linear')
+  """
+
+training_info_job = client.query(training_info)
+print(training_info_job)
+
+weights = """
+SELECT
+  category,
+  weight
+FROM
+  UNNEST((
+    SELECT
+      category_weights
+    FROM
+      ML.WEIGHTS(MODEL 'weather.linear')
+    WHERE
+      processed_input = 'input_col'))
+"""
+
+weights_info = client.query(weights)
+print(weights_info)
+
+
