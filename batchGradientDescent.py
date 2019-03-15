d0ae69be84acaa6ce7cf0e70c3028c589e31e50d
Patrick McNamee <p678m854@gmail.com>
2019-03-15 13:52:59 -0500

    Add files via upload

--- batchGradientDescent.py
+++ batchGradientDescent.py
@@ -2,21 +2,41 @@ import numpy as np
 
 X = np.array([[0, 1],[1, 1],[2,1]])
 Y = np.array([-1,2,2])
+Xnew = np.array([[3,1], [4,1],[5,1]])
+Ynew = np.array([3,4,5])
 
-def CFgradient(X, Y, theta):
-  j = X.shape[1] #numper of coefficients/regressors
-  m = X.shape[0] #number of points
-  gradient = np.zeros(j)
+class batchLSE:
+  def LSE(self, X, Y):
+    return np.matmul(np.matmul(np.linalg.inv(np.matmul(np.transpose(X),X)),np.transpose(X)),Y)
 
-  for j in range(j):
-    sumTotal = 0
-    for i in range(m):
-      sumTotal = sumTotal + (X[i,:]*theta - Y[i])*X[i,j]
-    gradient[j] = sumTotal/m
+  #Constructor
+  def __init__(self, X, Y, alpha = 0):
+    self.theta = self.LSE(X, Y)
+    self.alpha = alpha
 
-  return gradient
+  def setLearningRate(self, alpha):
+    self.alpha = alpha
+    
+  def CFgradient(self, X, Y):
+    j = X.shape[1] #numper of coefficients/regressors
+    m = X.shape[0] #number of points
+    gradient = np.zeros(j)
 
-theta = np.matmul(np.matmul(np.linalg.inv(np.matmul(np.transpose(X),X)),np.transpose(X)),Y)
+    for j in range(j):
+      sumTotal = 0
+      for i in range(m):
+        sumTotal = sumTotal + (np.matmul(X[i,:],self.theta) - Y[i])*X[i,j]
+        gradient[j] = sumTotal/m
 
-print(theta)
-print(CFgradient(X,Y,theta))
+    return gradient
+
+  def batchUpdate(self, Xnew, Ynew):
+    gradient = self.CFgradient(Xnew, Ynew)
+    for j in range(gradient.shape[0]):
+      self.theta[j] = self.theta[j] - (self.alpha * gradient[j])
+
+bLSE = batchLSE(X,Y)
+print(bLSE.theta)
+bLSE.setLearningRate(0.1)
+bLSE.batchUpdate(Xnew, Ynew)
+print(bLSE.theta)
